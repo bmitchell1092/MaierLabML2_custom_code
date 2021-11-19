5c97d69c22bd8dd383a691cf386f0d4c35bf698b
bmitchell1092 <53583871+bmitchell1092@users.noreply.github.com>
2021-11-19 12:43:47 -0600

    added another (experimental) textfile

--- 33 binocular gratings/T_binocular_gratings.m
+++ 33 binocular gratings/T_binocular_gratings.m
@@ -75,12 +75,12 @@ if tr == 1 % on the first trial
     
     % generate fixation cross location
     genFixCross((fixpt(1)*Screen.PixelsPerDegree), (fixpt(2)*Screen.PixelsPerDegree));
+   
     
     % Create a file to write grating information for each trial
-
-    filename = strcat(SAVEPATH,'/',datafile,'.g',upper(paradigm),'Grating_di');
+    filename_1 = strcat(SAVEPATH,'/',datafile,'.g',upper(paradigm),'Grating_di');
     
-    fid = fopen(filename, 'w');
+    fid = fopen(filename_1, 'w');
     formatSpec =  '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n';
     fprintf(fid,formatSpec,...
         'trial',...
@@ -103,6 +103,47 @@ if tr == 1 % on the first trial
         'header',...
         'grating_phase',...
         'path',...
+        'timestamp');
+    
+    fclose(fid);
+    
+    % Textfile # 2 (beta)
+    
+    filename_2 = strcat(SAVEPATH,'\',datafile,'.g',upper(paradigm),'Grating');
+
+    fid = fopen(filename_2, 'w');
+    formatSpec =  '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\r\n';
+    fprintf(fid,formatSpec,...
+        'trial',...             % Trial number
+        'header',...            % 'paradigm'
+        'horzdva',...           % dva from fused fixation
+        'vertdva',...           % dva from fused fixation
+        'xpos_L',...            % actual x-position of grating in the LE
+        'xpos_R',...            % actual x-position of grating in the RE
+        'ypos_L',...            % actual y-position of grating in the LE
+        'ypos_R',...            % actual y-position of grating in the RE
+        'x_disparity',...       % difference (in visual deg) between LE and RE grating x-positions
+        'y_disparity',...       % difference (in visual deg) between LE and RE grating y-positions
+        'contrast_L',...        % Michelson contrast of grating in the LE
+        'contrast_R',...        % Michelson contrast of grating in the RE
+        'ori_L',...             % orientation (tilt) of grating in the LE
+        'ori_R',...             % orientation (tilt) of grating in the RE
+        'phase_L',...           % Phase angle of grating in the LE
+        'phase_R',...           % Phase angle of grating in the RE
+        'sf_L',...              % Spatial frequency (cyc/deg) of grating in the LE
+        'sf_R',...              % Spatial frequency (cyc/deg) of grating in the RE
+        'tf_L',...              % Temporal frequency (cyc/deg/sec) of grating in the LE
+        'tf_R',...              % Temporal frequency (cyc/deg/sec) of grating in the RE
+        'diameter_L',...        % Diameter (size) of grating in the LE
+        'diameter_R',...        % Diameter (size) of grating in the RE
+        'trialHasBlank',...     % whether this trial has a blank presentation
+        'blank',...             % whether this presentation was a blank or not
+        'gabor',...             % whether the grating was gabor filtered
+        'gabor_std',...         % standard deviation of the gabor filter
+        'eye',...               % 1 = LE, 2 = RE, 3 = Both eyes
+        'duration',...          % stimulus duration
+        'isi',...               % interstimulus interval
+        'pathw',...             % for cone isolation
         'timestamp');
     
     fclose(fid);
@@ -518,10 +559,11 @@ set_iti(800); % Inter-trial interval in [ms]
 
 %% Write info to file
 
-filename = strcat(SAVEPATH,'\',datafile,'.g',upper(paradigm),'Grating_di');
+filename_1 = strcat(SAVEPATH,'\',datafile,'.g',upper(paradigm),'Grating_di');
+filename_2 = strcat(SAVEPATH,'\',datafile,'.g',upper(paradigm),'Grating');
     
 for pres = 1:prespertr
-    fid = fopen(filename, 'a'); % append
+    fid = fopen(filename_1, 'a'); % append
     formatSpec =  '%04u\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%u\t%f\t%s\t%f\t%f\t%f\r\n';
     fprintf(fid,formatSpec,...
         TrialRecord.CurrentTrialNumber,...
@@ -547,6 +589,47 @@ for pres = 1:prespertr
         now);
     
     fclose(fid);
+    
+    
+    % Textfile # 2 (beta)
+    
+    fid = fopen(filename_2, 'a'); % append
+    formatSpec =  '%04u\t%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\r\n';
+    fprintf(fid,formatSpec,...
+        TrialRecord.CurrentTrialNumber,...
+        grating_header,...             % 'paradigm'
+        grating_xpos(pres),...           % dva from fused fixation
+        grating_ypos(pres),...           % dva from fused fixation
+        stereo_xpos(pres)',...            % actual x-position of grating in the LE
+        other_stereo_xpos(pres),...            % actual x-position of grating in the RE
+        grating_ypos(pres),...            % actual y-position of grating in the LE
+        other_ypos(pres),...            % actual y-position of grating in the RE
+        grating_posdist(pres),...       % difference (in visual deg) between LE and RE grating x-positions
+        grating_phzdist(pres),...       % difference (in visual deg) between LE and RE grating y-positions
+        grating_fixedc(pres),...        % Michelson contrast of grating in the LE
+        grating_contrast(pres),...        % Michelson contrast of grating in the RE
+        gratL_tilt(pres),...             % orientation (tilt) of grating in the LE
+        gratR_tilt(pres),...             % orientation (tilt) of grating in the RE
+        gratL_phase(pres),...           % Phase angle of grating in the LE
+        gratR_phase(pres),...           % Phase angle of grating in the RE
+        grating_sf(pres),...              % Spatial frequency (cyc/deg) of grating in the LE
+        grating_sf(pres),...              % Spatial frequency (cyc/deg) of grating in the RE
+        grating_tf(pres),...              % Temporal frequency (cyc/deg/sec) of grating in the LE
+        grating_tf(pres),...              % Temporal frequency (cyc/deg/sec) of grating in the RE
+        grating_diameter(pres),...        % Diameter (size) of grating in the LE
+        grating_diameter(pres),...        % Diameter (size) of grating in the RE
+        0,...     % whether this trial has a blank presentation
+        0,...             % whether this presentation was a blank or not
+        0,...             % whether the grating was gabor filtered
+        0,...         % standard deviation of the gabor filter
+        grating_eye(pres),...               % 1 = LE, 2 = RE, 3 = Both eyes
+        grating_stimdur,...          % stimulus duration
+        grating_isi,...               % interstimulus interval
+        nan,...             % for cone isolation
+        now);
+    
+    fclose(fid);
+    
 end
 
     
